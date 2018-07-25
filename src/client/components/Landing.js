d37873227774aea86b029a710be8f48ed48a6c81
Rebecca Turk <rebeccaturk@Rebeccas-MacBook-Pro-2.local>
2018-07-25 16:41:33 -0400

    added some landing, signup, login styles

--- src/client/components/Landing.js
+++ src/client/components/Landing.js
@@ -11,13 +11,20 @@ class Landing extends Component {
     return (
       <div className="landing">
         <div className="landing__content">
-          <h1 className="landing__title">Axolotl</h1>
-          <Link to="/signup" className="landing__signup">
-            Get Started
-          </Link>
-          <Link to="/login" className="landing__login">
-            Log In
-          </Link>
+          <h1 className="landing__title">Awesome Axolotl Chat App</h1>
+          <iframe src="https://giphy.com/embed/RE5iREBNhI0Ok" width="200" height="200" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/weird-RE5iREBNhI0Ok"></a></p>
+          <div className="wrapper">
+            <div className="landing__signup">
+              <Link to="/signup">
+                Get Started
+              </Link>
+            </div>
+            <div>
+              <Link to="/login" className="landing__login">
+                Log In
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
